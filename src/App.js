305cde83c7614340b5913ed6c787dcbf99473554
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-09-01 19:37:52 -0500

    searching weather by city working

--- src/App.js
+++ src/App.js
@@ -55,26 +55,42 @@ function App() {
   };
 
   return (
-    <div className="app-warm">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
             type="text"
             className="search-bar"
-            placeholder="search..."
+            placeholder="Search..."
             onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">{weather.name},</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15°c</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
