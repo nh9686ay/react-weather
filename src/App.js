2cb685e73b75b731210764c6b71ec6cf56529000
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-09-01 19:27:39 -0500

    api json data requested

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
   const dateBuilder = (d) => {
     let months = [
       "January",
